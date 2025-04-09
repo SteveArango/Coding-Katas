fb5bcd0c0c9ad2ef3746778677505fc3b78a9b91
SteveArango <steve.arango1@gmail.com>
2025-04-09 10:07:41 -0500

    Add basic operations and FizzBuzz tests; update package configurations

---
+++ Basic Operations/basicOperations.test.ts
@@ -0,0 +1,40 @@
+import { describe, it, expect } from 'vitest';
+
+describe('Hello World Test', () => {
+    it('should return hello world', () => {
+    const message = 'hello world';
+    expect(message).toBe('hello world');
+    });
+});
+
+describe("adding numbers in js/ts", () => {
+    it("uses the add function", () => {
+    function add(a: number, b: number){
+        return a + b;
+    }
+    expect(add(5, 2)).toBe(7);
+    });
+    it("Uses the add function with negative numbers", () => {
+    function addNegatives(a: number, b:number){
+        return a + b;
+    }
+    expect(addNegatives(-5, -5)).toBe(-10);
+    });
+    it("Subtract numbers", () => { 
+    function subtraction(a: number, b: number){
+        return a - b
+    }
+    expect(subtraction(7,7)).toBe(0)
+    });
+
+    it("adding uses the + sign", () => {
+    const result = 1 + 2;
+    expect(result).toBe(3);
+    });
+
+    it
+
+
+
+
+});
--- example.test.ts
+++
@@ -1,8 +0,0 @@
-import { describe, it, expect } from 'vitest';
-
-describe('Hello World Test', () => {
-    it('should return hello world', () => {
-        const message = 'hello world';
-        expect(message).toBe('hello world');
-    });
-});
