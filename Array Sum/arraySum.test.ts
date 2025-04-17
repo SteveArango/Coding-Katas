d291458fb4a9e652b49f3b38040d6ab95c224743
SteveArango <steve.arango1@gmail.com>
2025-04-17 16:42:35 -0500

    Refactor sum function to simplify logic and improve readability; update tests accordingly

--- Array Sum/arraySum.test.ts
+++ Array Sum/arraySum.test.ts
@@ -19,4 +19,20 @@ describe('Function to calculate the sum of an array of numbers.', () => {
         //Assert
         expect(result).toBe(5);
     });
+    it('returns the sum of two numbers', () => {
+        //Arrange
+        const addTwoNumbers: number[] = [7,7];
+        //Act
+        const result = sum(addTwoNumbers);
+        //Assert
+        expect(result).toBe(14)
+    });
+    it('returns the sum of more than 3 numbers', () => {
+        //Arrange
+        const sumSeveralNumbers: number[] = [3,6,9,12]
+        //Act
+        const result = sum(sumSeveralNumbers);
+        //Assert
+        expect(result).toBe(30)
+    });
 });
--- Array Sum/arraySum.ts
+++ Array Sum/arraySum.ts
@@ -1,9 +1,3 @@
-export function sum(numbers: number[]): number {
-    if (numbers.length === 0) {
-        return 0;
-    }
-    if (numbers.length === 1) {
-        return numbers[0];
-    }
+export function sum(numbers: number[]): number  {
     return numbers.reduce((acc, curr) => acc + curr, 0);
 }
