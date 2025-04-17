cfcbaede3b6ed5812d66dc0ddc74e5ce34755015
SteveArango <steve.arango1@gmail.com>
2025-04-17 18:16:18 -0500

    Add return type to addTwo function and update console log examples.
    Add .map() method examples

--- tsBasics/functions.ts
+++ tsBasics/functions.ts
@@ -1,6 +1,6 @@
 // Function to add 2 to any number
 
-function addTwo(num: number) {
+function addTwo(num: number): number {
     return num + 2;
 }
 addTwo(7);
@@ -10,7 +10,7 @@ addTwo(7);
 function convertUpper(str: string){
     return str.toUpperCase();
 }
-//console.log(convertUpper("A la flaca le gusta Steve"));
+//console.log(convertUpper("a string"));
 
 // function signUpUser(name: string, email: string, isPaid: boolean) {
 //     console.log(`User name: ${name}`),
@@ -32,3 +32,40 @@ let loginUser = (name: string = 'Guest', email: string) => {
 console.log(loginUser('Steve', 'test'))
 console.log(loginUser(undefined, 'test@test.com'))
 
+    function getValue(myVal: number): number | string {
+    if (myVal === 0) {
+        return 'Steve is a warrior'
+    }
+    return myVal
+}
+
+const value = getValue(7)
+console.log(`This is my value: ${value}`)
+
+const arrowFunction = (number: number): number => {return number}
+
+const triple = arrowFunction(777)
+
+console.log(triple)
+
+const heroes = ['Don Juan', 'Genaro', 'Pablo', 'Nawal', 'Dante']
+
+const heroeMessage = heroes.map((hero: string) => hero + ' is awesome');
+
+console.log(heroeMessage)
+
+//.map() Method Examples
+
+
+const numbers = [3, 6, 9]
+
+const numbersMultiplied = numbers.map((numbers: number) => numbers * 3);
+
+console.log(numbersMultiplied)
+
+
+const list = [2,4,6,8,10]
+
+const addTwoToList = list.map((list: number) => list + 2);
+
+console.log(addTwoToList)
